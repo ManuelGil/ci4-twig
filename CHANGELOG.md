f79d9695992c6be9baef883fbd5f7d0566b67c92
ManuelGil <atencion.ms@outlook.com>
2022-10-06 21:30:24 -0500

    first commit

---
+++ CHANGELOG.md
@@ -0,0 +1,27 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+## [1.0.0] - 2022-09-06
+
+### Added
+
+-   Add Twig Template Engine library dependencies.
+-   Add Twig classes and render function to Base Controller.
+-   Add Twig Functions to use in templating.
+-   Add Template Partials to Modularize HTML.
+-   Add example route for use of javascript
+
+### Changed
+
+-   Update Home Controller to use render function.
+-   Rename welcome_message.php to welcome_message.twig.
+-   Convert welcome_message.twig to child of layout.twig.
+
+[unreleased]: https://github.com/ManuelGil/ci4-twig/compare/v1.0.0...HEAD
+[1.0.0]: https://github.com/ManuelGil/ci4-twig/releases/tag/v1.0.0
---
+++ app/Controllers/BaseController.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+use CodeIgniter\HTTP\CLIRequest;
+use CodeIgniter\HTTP\IncomingRequest;
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+use Psr\Log\LoggerInterface;
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+use Twig\Extension\DebugExtension;
+use Twig\Loader\FilesystemLoader;
+use Twig\TwigFunction;
+
+/**
+ * Class BaseController
+ *
+ * BaseController provides a convenient place for loading components
+ * and performing functions that are needed by all your controllers.
+ * Extend this class in any new controllers:
+ *     class Home extends BaseController
+ *
+ * For security be sure to declare any new methods as protected or private.
+ */
+abstract class BaseController extends Controller
+{
+    /**
+     * Instance of the main Request object.
+     *
+     * @var CLIRequest|IncomingRequest
+     */
+    protected $request;
+
+    private $templateEngine;
+
+    /**
+     * An array of helpers to be loaded automatically upon
+     * class instantiation. These helpers will be available
+     * to all other controllers that extend BaseController.
+     *
+     * @var array
+     */
+    protected $helpers = [];
+
+    /**
+     * Constructor.
+     */
+    public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
+    {
+        // Do Not Edit This Line
+        parent::initController($request, $response, $logger);
+
+        // Preload any models, libraries, etc, here.
+
+        // E.g.: $this->session = \Config\Services::session();
+
+        $loader = new FilesystemLoader(APPPATH . 'Views');
+
+        if (ENVIRONMENT === 'production') {
+            $this->templateEngine = new Environment($loader, [
+                'debug' => false,
+                'cache' => false,
+            ]);
+        } else {
+            $this->templateEngine = new Environment($loader, [
+                'debug' => true,
+                'cache' => false,
+            ]);
+            $this->templateEngine->addExtension(new DebugExtension());
+        }
+
+        // Create a new function for date.
+        $this->templateEngine->addFunction(new TwigFunction('date', function ($format) {
+            return date($format);
+        }));
+
+        // Create a new function for base_url.
+        $this->templateEngine->addFunction(new TwigFunction('base_url', function ($uri) {
+            return base_url($uri);
+        }));
+
+        // Create a new function for route_to.
+        $this->templateEngine->addFunction(new TwigFunction('route_to', function ($uri) {
+            return route_to($uri);
+        }));
+
+        // Create a new function for get_segment.
+        $this->templateEngine->addFunction(new TwigFunction('get_segment', function ($segment) {
+            return service('uri')->setSilent()->getSegment($segment);
+        }));
+
+        // Create a new function for session.
+        $this->templateEngine->addFunction(new TwigFunction('session', function ($key = null) {
+            return session($key);
+        }));
+
+        // Create a new function for old.
+        $this->templateEngine->addFunction(new TwigFunction('old', function ($key) {
+            return old($key);
+        }));
+
+        // Create a new function for set_value.
+        $this->templateEngine->addFunction(new TwigFunction('set_value', function ($field, $default) {
+            return set_value($field, $default);
+        }));
+
+        // Create a new function for csrf_token.
+        $this->templateEngine->addFunction(new TwigFunction('csrf_token', function () {
+            return csrf_token();
+        }));
+
+        // Create a new function for csrf_hash.
+        $this->templateEngine->addFunction(new TwigFunction('csrf_hash', function () {
+            return csrf_hash();
+        }));
+
+        // Create a new function for csrf_field.
+        $this->templateEngine->addFunction(new TwigFunction('csrf_field', function () {
+            return csrf_field();
+        }));
+    }
+
+    /**
+     * This method render the template.
+     *
+     * @param string $filename - the filename of template.
+     * @param array $params - the data with context of the template.
+     * @return void
+     */
+    public function render(string $filename, array $params = [])
+    {
+        try {
+            // Render the template.
+            return $this->templateEngine->render($filename, $params);
+        } catch (LoaderError | SyntaxError | RuntimeError | \Throwable $e) {
+            if (ENVIRONMENT === 'production') {
+                // Save error in file log
+                log_message('error', $e->getTraceAsString());
+            } else {
+                // Show error in the current page
+                header_remove();
+                http_response_code(500);
+                header('HTTP/1.1 500 Internal Server Error');
+                echo '<pre>' . $e->getTraceAsString() . '</pre>';
+                echo PHP_EOL;
+                echo $e->getMessage();
+                exit;
+            }
+        }
+    }
+}
